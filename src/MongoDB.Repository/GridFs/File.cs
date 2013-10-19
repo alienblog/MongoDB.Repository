d61288fe4b26fcc1f91998d167ff3af0e3792da1
alienblog <admin@alienblog.net>
2013-10-20 01:28:08 +0800

    添加注释

--- src/MongoDB.Repository/GridFs/File.cs
+++ src/MongoDB.Repository/GridFs/File.cs
@@ -3,27 +3,35 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoDB.Repository.GridFs
 {
+    /// <summary>
+    ///     文件类
+    /// </summary>
     public class File
     {
-        [BsonIgnore]
-        private byte[] _data;
+        [BsonIgnore] private byte[] _data;
 
+        /// <summary>
+        ///     文件名
+        /// </summary>
         public string FileName { get; set; }
 
+        /// <summary>
+        ///     文件所在GridFs的ObjectId
+        /// </summary>
         public ObjectId FileObjectId { get; set; }
 
+        /// <summary>
+        ///     文件所在GridFs的Id
+        /// </summary>
         public string Id
         {
-            get
-            {
-                return FileObjectId.ToString();
-            }
-            set
-            {
-                FileObjectId = ObjectId.Parse(value);
-            }
+            get { return FileObjectId.ToString(); }
+            set { FileObjectId = ObjectId.Parse(value); }
         }
 
+        /// <summary>
+        ///     文件数据
+        /// </summary>
         [BsonIgnore]
         public byte[] Data
         {
@@ -31,26 +39,32 @@ namespace MongoDB.Repository.GridFs
             {
                 if (_data == null)
                 {
-                    if(string.IsNullOrEmpty(Id))return new byte[0];
+                    if (string.IsNullOrEmpty(Id)) return new byte[0];
                     var repository = new GridfsRepository();
                     _data = repository.Find(Id).Data;
                 }
                 return _data;
             }
-            set
-            {
-                _data = value;
-            }
+            set { _data = value; }
         }
 
+        /// <summary>
+        ///     文件MD5码
+        /// </summary>
         public string MD5 { get; set; }
 
+        /// <summary>
+        ///     保存文件
+        /// </summary>
         public void Save()
         {
             var repository = new GridfsRepository();
             repository.Create(this);
         }
 
+        /// <summary>
+        ///     删除文件
+        /// </summary>
         public void Remove()
         {
             if (string.IsNullOrEmpty(Id)) return;
